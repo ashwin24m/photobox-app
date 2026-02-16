e71eb207dee99764a505030d550d40d0f985dc54
Ashwin <142091652+ashwin24m@users.noreply.github.com>
2026-02-16 12:04:45 +0000

    use env variables

--- photobox/lib/supabase.ts
+++ photobox/lib/supabase.ts
@@ -1,7 +1,20 @@
+// import { createClient } from '@supabase/supabase-js'
+
+// const supabaseUrl = 'https://qsissqdraatvdhscrxog.supabase.co'
+
+// const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFzaXNzcWRyYWF0dmRoc2NyeG9nIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzEyMTk1NzIsImV4cCI6MjA4Njc5NTU3Mn0.P2UmmVgNic18pMUTNydClau7BwAFnUyIDKZSkDcD1vs'
+
+// export const supabase = createClient(supabaseUrl, supabaseKey)
+
+
+
+
+
+
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'https://qsissqdraatvdhscrxog.supabase.co'
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!
 
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFzaXNzcWRyYWF0dmRoc2NyeG9nIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzEyMTk1NzIsImV4cCI6MjA4Njc5NTU3Mn0.P2UmmVgNic18pMUTNydClau7BwAFnUyIDKZSkDcD1vs'
+const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
 
 export const supabase = createClient(supabaseUrl, supabaseKey)
