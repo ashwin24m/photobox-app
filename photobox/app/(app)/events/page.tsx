a8834485b20e11f8e5f57bee6d41f6a773554ee4
Ashwin <142091652+ashwin24m@users.noreply.github.com>
2026-02-19 12:07:15 +0000

    Changed for the deploy error UIUX changesv5

--- photobox/app/(app)/events/page.tsx
+++ photobox/app/(app)/events/page.tsx
@@ -43,6 +43,7 @@ useEffect(() => {
 
 
 
+
   async function initialize() {
 
   const {
--- photobox/lib/supabase.ts
+++ photobox/lib/supabase.ts
@@ -1,17 +1,3 @@
-// import { createClient } from '@supabase/supabase-js'
-
-// const supabaseUrl = 'https://qsissqdraatvdhscrxog.supabase.co'
-
-// const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFzaXNzcWRyYWF0dmRoc2NyeG9nIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzEyMTk1NzIsImV4cCI6MjA4Njc5NTU3Mn0.P2UmmVgNic18pMUTNydClau7BwAFnUyIDKZSkDcD1vs'
-
-// export const supabase = createClient(supabaseUrl, supabaseKey)
-
-
-
-
-
-"use client";
-
 import { createClient } from "@supabase/supabase-js";
 
 let supabaseClient: any = null;
